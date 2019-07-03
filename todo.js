eed19d9397365f72396f147de0e924fd2a82a52d
Ashley Kung <ashley.berg@gmail.com>
2019-07-02 20:27:03 -0400

    Add todoList object and methods

---
+++ todo.js
@@ -0,0 +1,61 @@
+//To Do List App
+//Will be improved iteratively as I gain advanced JS skills.
+
+let todoList = {
+  todos: [],
+  
+  displayTodos: function() {
+    if (this.todos.length === 0) {
+      console.log("To do list is empty.");
+    } else {
+      console.log("My To Dos:");
+      for (let i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+          console.log('(x) ' + this.todos[i].item);
+        } else {
+          console.log('( ) ' + this.todos[i].item);
+        };
+      };
+    };
+  },
+  
+  addTodo: function(item) {
+    this.todos.push({ item: item, completed: false });
+    this.displayTodos();
+  },
+  
+  changeTodo: function(itemIndex, newValue) {
+    this.todos[itemIndex].item = newValue;
+    this.displayTodos();
+  },
+  
+  toggleCompleted: function(itemIndex) {
+    let todo = this.todos[itemIndex];
+    todo.completed = !todo.completed;
+  },
+  
+  toggleAll: function() {
+    var totalToDos = this.todos.length;
+    var completedToDos = 0;
+    for (let i = 0; i < this.todos.length; i++) {
+      if (this.todos[i].completed === true) {
+        completedToDos++;
+      }
+    }
+    if (totalToDos === completedToDos) {
+    for (let j = 0; j < this.todos.length; j++) {
+      this.todos[j].completed = false;
+    }
+    } else {
+      for (let k = 0; k < this.todos.length; k++) {
+        this.todos[j].completed = true;
+      }
+    }
+    this.displayTodos();
+  },
+  
+  deleteTodo: function(itemIndex) {
+    this.todos.splice(itemIndex, 1);
+    this.displayTodos();
+  }
+};
